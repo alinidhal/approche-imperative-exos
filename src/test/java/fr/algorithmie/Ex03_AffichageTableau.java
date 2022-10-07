8bf991d2cd58e023ba38456fae3e022b3bc61130
Ali NIDHAL <ali.nidhal@hotmail.com>
2022-10-07 17:43:33 +0200

    Exo3

--- src/test/java/fr/algorithmie/Ex03_AffichageTableau.java
+++ src/test/java/fr/algorithmie/Ex03_AffichageTableau.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import outils.Exercice;
 import outils.ExerciceRunner;
 import outils.Question;
+import outils.Resultat;
 
 /** Ne modifiez ni les noms des classes, ni les noms des méthodes.
  * Utilisez Resultat.log pour afficher les résultats
@@ -24,12 +25,29 @@ public class Ex03_AffichageTableau {
 	public void affichageTableau() {
 		
 		//TODO LOGUER les éléments du tableau grâce à une boucle
+		
+		Resultat.log("Question 1: Éléments du tableau\n");
+		
+		for (int i =0; i<=array.length-1; i++) {
+			
+			Resultat.log(array[i]);
+		}
+	
 	}
+		
 	
 	@Test
 	@Question(numero=2)
 	public void affichageTableauOrdreInverse() {
 		
 		//TODO LOGUER les éléments du tableau dans l'ordre inverse
+		
+		Resultat.log("\nQuestion 2: Éléments du tableau ordre inverse\n");
+		
+		for (int i = (array.length-1); i>=0; i--) {
+			
+			Resultat.log(array[i]);
+		}
+		
 	}
 }
