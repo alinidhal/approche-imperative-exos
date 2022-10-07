f7177dc3f2b34cfceeaadcbb4d52ff108e5b0f67
Ali NIDHAL <ali.nidhal@hotmail.com>
2022-10-07 16:58:35 +0200

    Exo1 et Exo2

--- src/test/java/fr/algorithmie/Ex01_AffichageIdentite.java
+++ src/test/java/fr/algorithmie/Ex01_AffichageIdentite.java
@@ -25,9 +25,8 @@ public class Ex01_AffichageIdentite {
 	public void main() {
 		//TODO Utiliser une boucle pour afficher (avec Resultat.log) 10 fois un texte quelconque
 		
-		// TODO Exemple : 
 		for (int i=1; i<=10; i++) {
-			Resultat.log("Coucou");
+			Resultat.log("Hello world");
 		}
 	}
 
--- src/test/java/fr/algorithmie/Ex02_AffichagePartiel.java
+++ src/test/java/fr/algorithmie/Ex02_AffichagePartiel.java
@@ -26,6 +26,23 @@ public class Ex02_AffichagePartiel {
 		
 		//TODO En utilisant une boucle et un if, LOGUER avec Resultat.log 
 		// les valeurs du tableau supérieures ou égales à 3
+		
+		Resultat.log("\nQuestion 1: Sup ou = 3\n");
+		
+		// Pour i = 0, on parcours la longeur du tableau -1
+		
+		for (int i = 0; i <= array.length-1; i++) {
+		
+			// Si la valeur de i dans le tableau est supérieur ou égale à 3 
+			
+			if (array[i]>=3) {
+				
+				// Alors, on affiche cette valeur i du tableau (qui est >= 3)
+				
+				Resultat.log(array[i]);
+				
+			}
+		}
 	}
 	
 	@Test
@@ -33,6 +50,17 @@ public class Ex02_AffichagePartiel {
 	public void affichageValeursPaires() {
 		
 		//TODO LOGUER avec Resultat.log les valeurs paires du tableau (0 sera considéré comme pair)
+		
+		Resultat.log("\nQuestion 2: Valeur pair\n");
+		
+		for (int i=0; i<= array.length-1; i++) {
+			
+			if( (array[i] % 2 == 0) && (array[i]>=0) ) {
+				
+				Resultat.log(array[i]);
+		}
+			
+		}
 	}
 	
 	@Test
@@ -41,6 +69,17 @@ public class Ex02_AffichagePartiel {
 		
 		//TODO LOGUER avec Resultat.log les valeurs correspondant aux index pairs du tableau 
 		// (0 sera considéré comme pair)
+		
+		Resultat.log("\nQuestion 3: Valeur index pairs\n");
+		
+		for (int i=0; i<= array.length-1; i++) {
+			
+			if( array[i] % 2 == 0 ) {
+				
+				Resultat.log(array[i]);
+		}
+			
+		}
 	}
 	
 	@Test
@@ -49,5 +88,15 @@ public class Ex02_AffichagePartiel {
 		
 		//TODO LOGUER avec Resultat.log les valeurs impaires positives du tableau (0 doit être exclu)
 		
+		Resultat.log("\nQuestion 4: Valeurs impaires positif !=0\n");
+		
+		for (int i=0; i<= array.length-1; i++) {
+			
+			if( (array[i] % 2 != 0) && (array[i]>=0) ) {
+				
+				Resultat.log(array[i]);
+		}
+			
+		}
 	}
 }
